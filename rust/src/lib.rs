b47db406e5e0023904c6f02877233f2ccd5084bd
cam <cameronayres123@gmail.com>
2024-11-21 23:35:47 +0000

    Now rustc won't complain about uncalled functions.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,7 +3,8 @@ mod frb_generated;
 
 /* Ignore this function, it's just here
  * for testing purposes.
- */ 
+ */
+#[allow(dead_code)]
 fn rust_main(name: String) -> String {
     let mut message = String::from("hello, ");
     message.push_str(&name);
