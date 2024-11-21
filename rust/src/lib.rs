af5ad16b3bb20302a13f608bb1a765a74ea0dd7b
cam <cameronayres123@gmail.com>
2024-11-21 23:28:19 +0000

    Wrote a rust hello-world thing for testing.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,2 +1,10 @@
 pub mod api;
 mod frb_generated;
+
+
+fn rust_main(name: String) -> String {
+    let mut message = String::from("hello, ");
+    message.push_str(&name);
+    message.push_str("!");
+    message
+}
